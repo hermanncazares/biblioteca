7117d3dfdd08880f20268b1d1a427a94999872a4
Fabricio1352 <100040158+Fabricio1352@users.noreply.github.com>
2024-03-19 14:03:03 -0700

    Initial commit

---
+++ src/test/java/test/Pruebas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package test;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author fabri
+ */
+public class Pruebas {
+    
+    public Pruebas() {
+    }
+    
+    
+    @Test
+    public void autenticarUsuario(){
+//        assertEquals(usuario1, usuarioDao.buscarUsuario(usuario));
+    }
+
+   
+}
